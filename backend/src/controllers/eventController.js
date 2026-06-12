6f82c1f85ee79d57fc2a5266939bcd786ea9c874
Vinaya-16 <vinayapatole16@gmail.com>
2026-06-12 13:08:24 +0530

    Events CRUD Done

--- backend/src/controllers/eventController.js
+++ backend/src/controllers/eventController.js
@@ -9,7 +9,7 @@ export const createEvent = async (req, res) => {
             event
         });
     }
-    catch(error) {
+    catch (error) {
         res.status(500).json({
             message: error.message,
         });
@@ -18,7 +18,7 @@ export const createEvent = async (req, res) => {
 
 export const getEvents = async (req, res) => {
     try {
-        const events = await Event.find().sort({createdAt: -1});
+        const events = await Event.find().sort({ createdAt: -1 });
 
         res.status(200).json({
             success: true,
@@ -30,6 +30,89 @@ export const getEvents = async (req, res) => {
             success: false,
             message: "failed to fetch events",
             error: error.message
+        });
+    }
+};
+
+export const singleEvent = async (req, res) => {
+    try {
+        const event = await Event.findById(req.params.id);
+
+        if (!event) {
+            return res.status(401).json({
+                message: "No Such Event",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            event
+        });
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+export const updateEvent = async (req, res) => {
+    try {
+        const event = await Event.findById(req.params.id);
+
+        if (!event) {
+            return res.status(401).json({
+                success: false,
+                message: "No Such Event",
+            });
+        }
+
+        const updatedEvent = await Event.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+
+        res, status(200).json({
+            success: true,
+            event: updatedEvent,
+        });
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+export const deleteEvent = async (req, res) => {
+    try {
+        const event = await Event.findById(req.params.id);
+
+        if (!event) {
+            return res.status(401).json({
+                success: false,
+                message: "No such Event"
+            });
+        }
+
+        await Event.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            success: true,
+            message: "Event deleted successfully"
+        });
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
         });
     }
 };
--- backend/src/routes/eventRoute.js
+++ backend/src/routes/eventRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { createEvent, getEvents } from "../controllers/eventController.js";
+import { 
+    createEvent, 
+    getEvents,
+    singleEvent,
+    updateEvent,
+    deleteEvent
+ } from "../controllers/eventController.js";
 
 const router = express.Router();
 
 router.post("/create-event", createEvent);
 router.get("/", getEvents); 
+router.get("/event-by-id", singleEvent);
+router.put("/updated-event", updateEvent);
+router.delete("/deleted-event", deleteEvent);
+
 export default router;
